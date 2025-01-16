7cba50fa0921d576c69bda7477a9357a7238ea06
David Gelpi <gelpiorama@gmail.com>
2025-01-16 18:55:08 +0100

    refactor(test_automaton): Renombrar variable scoreCard

--- test/test_automaton.py
+++ test/test_automaton.py
@@ -9,8 +9,8 @@ def test_hitting_pins_regular():
     automata = Automaton()
     pins = "12345123451234512345"
     total = 60
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.state_n
@@ -19,15 +19,15 @@ def test_symbol_zero():
     automata = Automaton()
     pins = "9-9-9-9-9-9-9-9-9-9-"
     total = 90
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
     automata = Automaton()
     pins = "9-3561368153258-7181"
     total = 82
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.spare
@@ -36,8 +36,8 @@ def test_spare_not_extra():
     automata = Automaton()
     pins = "9-3/613/815/-/8-7/8-"
     total = 121
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.strike
@@ -46,25 +46,25 @@ def test_strike():
     automata = Automaton()
     pins = "X9-9-9-9-9-9-9-9-9-"
     total = 100
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
     automata = Automaton()
     pins = "X9-X9-9-9-9-9-9-9-"
     total = 110
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.strike
-def test_two_strikes(): 
+def test_two_strikes():
     # two strikes in a row is a double
     automata = Automaton()
     pins = "XX9-9-9-9-9-9-9-9-"
     total = 120
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.strike
@@ -73,8 +73,8 @@ def test_three_strikes():
     automata = Automaton()
     pins = "XXX9-9-9-9-9-9-9-"
     total = 141
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.extra_rolls
@@ -83,15 +83,15 @@ def test_one_pin_in_extra_roll():
     automata = Automaton()
     pins = "9-3/613/815/-/8-7/8/8"
     total = 131
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
     automata = Automaton()
     pins = "5/5/5/5/5/5/5/5/5/5/5"
     total = 150
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.extra_rolls
@@ -100,8 +100,8 @@ def test_two_strikes_in_extra_rolls():
     automata = Automaton()
     pins = "9-9-9-9-9-9-9-9-9-XXX"
     total = 111
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.extra_rolls
@@ -110,8 +110,8 @@ def test_one_strike_in_extra_roll():
     automata = Automaton()
     pins = "8/549-XX5/53639/9/X"
     total = 149
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.extra_rolls
@@ -120,8 +120,8 @@ def test_spare_in_extra_roll():
     automata = Automaton()
     pins = "X5/X5/XX5/--5/X5/"
     total = 175
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
 
 @pytest.mark.extra_rolls
@@ -131,6 +131,6 @@ def test_triple_strike_before_extra_rolls():
     automata = Automaton()
     pins = "XXXXXXXXXXXX"
     total = 300
-    scoreCard = ScoreCard(pins)
-    automata.set_input(scoreCard)
+    score_card = ScoreCard(pins)
+    automata.set_input(score_card)
     assert automata.output() == total
